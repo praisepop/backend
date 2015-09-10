696fb13c3fd2d4d1e9b020df91eb1bce4f0d0275
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-09-10 18:44:46 -0400

    Fixed server.js to allow our "custom" router

--- server.js
+++ server.js
@@ -15,30 +15,25 @@ if (app.get('env') == 'development') {
   // app.use(express.errorHandler());
 }
 
-app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // restrict it to the required domain
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Authorization,Content-type,Accept,X-Access-Token,X-Key');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
     next();
-  }
 });
 
-app.use('/', require('./app/routes/'));
+var router = express.Router();
+router = require('./app/routes/')(router); // configure our routes
 
-// If no route is matched by now, it must be a 404
-app.use(function(req, res, next) {
-  // var err = new Error('Not Found');
-  res.status(404).send('404');
-  next();
+app.use('/api/v1', router);
+
+app.use('*', function request(req, res) {
+  res.json({error: 'Nothing to see here, move along!'});
 });
 
 app.listen(app.get('port'), function(){
-  util.log('PraisePop API is up and running on port ' + app.get('port'));
+  console.log('PraisePop API is up and running on port ' + app.get('port'));
 });
 
 function logRequest(request) {
-	util.log(request.method + ' request for ' + request.path + ' from ' + request.hostname);
 }
