913d1c0daf924a5a3dd27134113a433e632e53b9
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-09-24 00:24:32 -0400

    Updated organization schema

--- app/models/organization.js
+++ app/models/organization.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var organizationSchema = new Schema({
   name: { type: String, required: false },
   domain: { type: String, required: true },
-  parent_org: [{ type: Boolean, required: true, default: true }],
+  parent_org: { type: Boolean, required: true, default: true },
   sub_orgs: [{ type: Schema.Types.ObjectId, ref:'Organization' }],
   created_by: { type: Schema.Types.ObjectId, ref: 'User' },
 	created_at: { type: Date, default: Date.now },
